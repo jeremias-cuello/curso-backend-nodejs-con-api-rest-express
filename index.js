b72e9fce7631cd824b1ab1ee254ecbde14063980
Jeremias <cuellojeremiasnatanael@gmail.com>
2025-03-16 00:05:18 -0300

    dev | routing

--- index.js
+++ index.js
@@ -3,19 +3,38 @@ const express = require("express");
 const app = express();
 const PORT = 3_000;
 
-app.get("/", (req, res) => {
-  res.send("Hola mi server en express");
-});
-
-app.get("/nueva-ruta", (req, res) => {
-  res.send("Hola, soy una nueva ruta");
-});
-
 app.get("/products", (req, res) => {
-  res.json({
-    name: "Producto 1",
-    price: 1_000,
-  });
+  const products = [
+    {id: 1, name: "Product 1", price: 150},
+    {id: 2, name: "Product 2", price: 3_210},
+    {id: 3, name: "Product 3", price: 1_450},
+    {id: 4, name: "Product 4", price: 1_150},
+  ];
+
+  res.json(products);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = {id, name: "Product Individual", price: 10_000};
+
+  res.json(product);
+});
+
+app.put("/products/:id", (req, res) => {
+  res.send("Producto reemplazado");
+});
+
+app.patch("/products/:id", (req, res) => {
+  res.send("Producto actualizado");
+});
+
+app.post("/products/:id", (req, res) => {
+  res.send("Producto creado");
+});
+
+app.delete("/products/:id", (req, res) => {
+  res.send("Producto eliminado");
 });
 
 app.listen(PORT, () => {
